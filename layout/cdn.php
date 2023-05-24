07a9ba9e606b4a926c678530cd23cadc15f9179a
peeyushsachan <sachana766@gmail.com>
2023-05-24 12:43:04 +0530

    seccond

---
+++ layout/cdn.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Keywords" content="yorur blog seo">
+<meta name="Description" content="seo">
+<link rel="icon" href="https://xpo.com/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="public/lib/x.css">
+<script src="https://kit.fontawesome.com/9522163cff.js" crossorigin="anonymous"></script>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
