efcc800341186a06b2e5309912ad7468122e0e08
Anmol Sahu <anmolsahu04012001@gmail.com>
2023-05-24 22:55:24 +0530

    Some Changes

--- layout/body.php
+++ layout/body.php
@@ -2,3 +2,4 @@ h2>Easy Learning with HTML &quot;Try it Yourself&quot;</h2>
 <p>With our &quot;Try it Yourself&quot; editor, you can edit the HTML code and view the
     result:</p>
 <h2>This Website is Developed by Mr. Peeyush Sachan</h2>
+<h3> Some Code Added by Anmol Sahu</h3>
