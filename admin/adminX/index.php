1bd5c0021519004a68a5b10e7dcdd2a9febedcc0
peeyushsachan <sachana766@gmail.com>
2023-05-24 13:51:50 +0530

    admin dashboard

--- admin/adminX/index.php
+++ admin/adminX/index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Login - SB Admin Pro</title>
+        <title>Login - SB Admin PRO</title>
         <link href="..\css\styles.css" rel="stylesheet">
         <link rel="icon" type="image/x-icon" href="..\assets\img\favicon.png">
         <script data-search-pseudo-elements="" defer="" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
