ff159869e41009af3e5d235a367e4ee16d6d964b
peeyushsachan <sachana766@gmail.com>
2023-05-24 18:04:25 +0530

    final

--- admin/adminX/index.php
+++ admin/adminX/index.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <title>Login - SB Admin PRO</title>
-        <link href="..\css\styles.css" rel="stylesheet">
+        <link href="../css/styles.css" rel="stylesheet">
         <link rel="icon" type="image/x-icon" href="..\assets\img\favicon.png">
         <script data-search-pseudo-elements="" defer="" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.27.0/feather.min.js" crossorigin="anonymous"></script>
--- admin/include/header-cdn.php
+++ admin/include/header-cdn.php
@@ -3,11 +3,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Dashboard - SB Admin Pro</title>
-        <link href="..\css\styles.css" rel="stylesheet">
+        <title>Dashboar - SB Admin Pro</title>
+        <link href="../css/styles.css" rel="stylesheet">
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" rel="stylesheet" crossorigin="anonymous">
-        <link rel="icon" type="image/x-icon" href="assets\img\favicon.png">
+        <link rel="icon" type="../image/x-icon" href="../assets/img/favicon.png">
         <script data-search-pseudo-elements="" defer="" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.27.0/feather.min.js" crossorigin="anonymous"></script>
 
