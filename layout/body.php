e4f9991686ed3924b4303f9a6a52414101f76433
Anmol Sahu <anmolsahu04012001@gmail.com>
2023-05-25 20:38:02 +0530

    hello

--- layout/body.php
+++ layout/body.php
@@ -1,7 +1,5 @@
-h2>Easy Learning with HTML &quot;Try it Yourself&quot;</h2>
+<h2>Easy Learning with HTML &quot;Try it Yourself&quot;</h2>
 <p>With our &quot;Try it Yourself&quot; editor, you can edit the HTML code and view the
     result:</p>
 <h2>This Website is Developed by Mr. Peeyush Sachan</h2>
-<h3> Some Code Added by Anmol Sahu </h3>
-<h2> Some Code Written by Anmol Sahu </h2>
-<h2> Some Code Written by peeyush sachan </h2>
+<h3>Anmol Sahu</h3>
