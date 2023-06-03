d914b283f70446df803c2552711cb608c1a355af
peeyushsachan <sachana766@gmail.com>
2023-06-03 17:14:02 +0530

    peeyush comit 221

--- layout/body.php
+++ layout/body.php
@@ -1,7 +1,16 @@
-<h2>Easy Learning with HTML &quot;Try it Yourself&quot;</h2>
-<p>With our &quot;Try it Yourself&quot; editor, you can edit the HTML code and view the
-    result:</p>
-<h2>This Website is Developed by Mr. Peeyush Sachan</h2>
-<h2>This Website is Developed by Mr. Anmol Sahu</h2>
-<h2>This Website is Developed by Mr. Anmol Sahu</h2>
-<h2>This Website is Developed by Mr. Anmol Sahu</h2>
+
+
+<?php 
+
+include('dbcon.php');
+
+
+ 
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+ echo "Connected successfully";
+?>
+
+
